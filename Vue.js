8e95597348ac087496dac7d1adbc3e8bef98e0ff
AlessandroSestili <sestilialessandro@gmail.com>
2021-05-24 22:34:07 +0200

    implemento links navbar

---
+++ Vue.js
@@ -0,0 +1,52 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      navbarLinks: [
+        {
+          text: "HOME",
+          link: "#",
+        },
+
+        {
+          text: "PAGES",
+          link: "#",
+        },
+
+        {
+          text: "PROGRAM",
+          link: "#",
+        },
+
+        {
+          text: "HOME",
+          link: "#",
+        },
+
+        {
+          text: "TICKETS",
+          link: "#",
+        },
+
+        {
+          text: "SPEAKERS",
+          link: "#",
+        },
+
+        {
+          text: "BLOG",
+          link: "#",
+        },
+
+        {
+          text: "SHORTCODES",
+          link: "#",
+        },
+
+        {
+          text: "xD",
+          link: "#",
+        },
+        
+      ]
+    }
+  })
