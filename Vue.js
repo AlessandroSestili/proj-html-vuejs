aa0c10280eaea41c9502fbfa668e57824433b075
AlessandroSestili <sestilialessandro@gmail.com>
2021-05-25 11:49:31 +0200

    implemento header navbar

--- Vue.js
+++ Vue.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: '#app',
+
     data: {
+
+      //Links nella Navbar
       navbarLinks: [
         {
           text: "HOME",
@@ -43,10 +46,26 @@ var app = new Vue({
         },
 
         {
-          text: "xD",
-          link: "#",
+          // Barra di ricerca 
+          text: "",
+          link: "",
         },
         
-      ]
+      ],
+
+      searchBarActive: false,
+    },
+
+    methods: {
+      // Apre o Chiude la barra di ricerca
+      openCloseSearchBar() {
+        this.searchBarActive = !this.searchBarActive
+
+        setTimeout(() => {
+          if(this.searchBarActive == true) {
+            this.searchBarActive = false
+          }
+        }, 4000);
+      }
     }
   })
